27efe7d1f17978955fdf5a9682f371dbbb6a9131
Inferno2401 <ameyavikramasingh@gmail.com>
2022-06-15 00:02:12 +0530

    fixed indentation in vec3.hpp

--- utilities/include/vec3.hpp
+++ utilities/include/vec3.hpp
@@ -86,90 +86,90 @@ class vec3{
 };
 
 constexpr vec3 operator+(const vec3& v1, const vec3& v2){
-            vec3 copy = v1;
-            copy += v2;
-            return copy;
+    vec3 copy = v1;
+    copy += v2;
+    return copy;
 
-        }
+}
 
 constexpr vec3 operator-(const vec3& v1, const vec3& v2){
-            vec3 copy = v1;
-            copy -= v2;
-            return copy;
-        }
+    vec3 copy = v1;
+    copy -= v2;
+    return copy;
+}
 
 constexpr vec3 operator*(const vec3& v1, const float& v2){
-            vec3 copy = v1;
-            copy *= v2;
-            return copy;
-        }
+    vec3 copy = v1;
+    copy *= v2;
+    return copy;
+}
 
 constexpr vec3 operator*(const float& s, const vec3& v1){
-            return v1*s;
+    return v1*s;
 }
 
 constexpr vec3 operator/(const vec3& v1, const float& s){
-            vec3 copy = v1;
-            copy /= s;
-            return copy;
-        }
+    vec3 copy = v1;
+    copy /= s;
+    return copy;
+}
 
 constexpr vec3& operator+=(vec3& v1, const vec3& v2){
-            v1.x+=v2.x;
-            v1.y+=v2.y;
-            v1.z+=v2.z;
-            return v1;
-        }
+    v1.x+=v2.x;
+    v1.y+=v2.y;
+    v1.z+=v2.z;
+    return v1;
+}
 
 constexpr vec3& operator-=(vec3& v1, const vec3& v2){
-            v1.x-=v2.x;
-            v1.y-=v2.y;
-            v1.z-=v2.z;
-            return v1;
-        }
+    v1.x-=v2.x;
+    v1.y-=v2.y;
+    v1.z-=v2.z;
+    return v1;
+}
 
 constexpr vec3& operator*=(vec3& v1, const float& s){
-            v1.x*=s;
-            v1.y*=s;
-            v1.z*=s;
-            return v1;
-        }
+    v1.x*=s;
+    v1.y*=s;
+    v1.z*=s;
+    return v1;
+}
 
 constexpr vec3& operator/=(vec3& v1, const float& s){
-            v1.x/=s;
-            v1.y/=s;
-            v1.z/=s;
-            return v1;
-        }
+    v1.x/=s;
+    v1.y/=s;
+    v1.z/=s;
+    return v1;
+}
 
 constexpr vec3 operator-(const vec3& v1) {
-            return v1*(-1);
-        }
+    return v1*(-1);
+}
 
 constexpr float dot(const vec3& v1, const vec3& v2){
-            return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
-        }
+    return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
 
 constexpr vec3 cross(const vec3& v1, const vec3& v2){
-            return {v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x };
-        }
+    return {v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x };
+}
 
 constexpr float angle(const vec3& v1, const vec3& v2){
-            float cos=(dot(v1,v2))/(v1.norm()*v2.norm());
-            return acos(cos);
-        }
+    float cos=(dot(v1,v2))/(v1.norm()*v2.norm());
+    return acos(cos);
+}
 
 constexpr vec3 proj(const vec3& v1, const vec3& v2){
-            return v2*(dot(v1,v2))/(v2.norm()*v2.norm());                       
-        }
+    return v2*(dot(v1,v2))/(v2.norm()*v2.norm());                       
+}
 
 constexpr vec3 perp(const vec3& v1, const vec3& v2){
-            return v1 - proj(v1,v2);
-        }
+    return v1 - proj(v1,v2);
+}
 
 std::ostream& operator<<(std::ostream& out, const vec3& vect){
-            out<<"("<<vect.x<<","<<vect.y<<","<<vect.z<<")\n";
-            return out;
-        }
+    out<<"("<<vect.x<<","<<vect.y<<","<<vect.z<<")\n";
+    return out;
+}
 
 #endif
