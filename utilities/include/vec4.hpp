c57cae9f9ce6fc8a66d4e4fd15a4a78ae62109b1
Prateek Garg <prateekgarg0020@gmail.com>
2022-07-01 22:03:08 +0530

    Update vec4
    incorporated changes requested in the PR #(11)
    explicit single value constructor
    renaming a to w
    [] operator signature
    use sum function
    made post-multiplication less terse
    single quotes for single chars
    added description for b2v and v2b
    updated the sum func itself

--- utilities/include/vec4.hpp
+++ utilities/include/vec4.hpp
@@ -19,10 +19,10 @@ template<class T, class A = xsimd::default_arch>
 class alignas(ALIGN_WIDTH) vec4{
     public:
         constexpr vec4() : m_v{0,0,0,0} {}
-        constexpr vec4(T x) : m_v{x,x,x,x} {}
+        constexpr explicit vec4(T x) : m_v{x,x,x,x} {}
         constexpr vec4(T x,T y,T z) : m_v{x,y,z,0} {}
-        constexpr vec4(T x,T y,T z,T a) : m_v{x,y,z,a} {}
-        constexpr vec4(xsimd::batch<T,A> x){x.store_aligned(m_v);}
+        constexpr vec4(T x,T y,T z,T w) : m_v{x,y,z,w} {}
+        constexpr explicit vec4(xsimd::batch<T,A> x){x.store_aligned(m_v);}
         
         //getters
         constexpr T x() const{
@@ -37,28 +37,34 @@ class alignas(ALIGN_WIDTH) vec4{
             return m_v[2];
         }
 
-        constexpr T a() const{
+        constexpr T w() const{
             return m_v[3];
         }
         
+        //indexing operation
+        constexpr T const & operator[](std::size_t i ) const{
+            return m_v[i];
+        }
+        
 
-        constexpr T dot(const vec4& v2,const bool alpha=ALPHA_) const{
-            return m_v[0]*v2.m_v[0]+m_v[1]*v2.m_v[1]+m_v[2]*v2.m_v[2]+(m_v[3]*v2.m_v[3])*alpha;
+        constexpr T dot(const vec4& v2) const{
+            auto temp = v2b(*this)*v2b(v2);
+            return sum(temp); //returns the sum of first 3 elements.
         }
         
         constexpr T norm(const bool alpha=ALPHA_) const{ //__GNUC__
-            return sqrt(dot(*this,alpha));
+            return sqrt(dot(*this));
         }
 
         constexpr static T angle(const vec4& v1,const vec4& v2,const bool alpha=ALPHA_){//__GNUC__
-            return acos(v1.dot(v2,alpha)/(v1.norm(alpha)*v2.norm(alpha)));
+            return acos(v1.dot(v2)/(v1.norm(alpha)*v2.norm(alpha)));
         }
         constexpr vec4 unit() const{//__GNUC__
             return *this/this->norm();
         }
 
         
-        constexpr vec4 cross(const vec4& v2) const{ //ignoring the "alpha" channel
+        constexpr vec4 cross(const vec4& v2) const{
             return vec4(m_v[1] * v2.m_v[2] - m_v[2] * v2.m_v[1], m_v[2] * v2.m_v[0] - m_v[0] * v2.m_v[2],m_v[0] * v2.m_v[1] - m_v[1] * v2.m_v[0],0);
         }
 
@@ -74,7 +80,8 @@ class alignas(ALIGN_WIDTH) vec4{
 
         //scalar post-multiplication
         friend constexpr vec4 operator*(const vec4& v1, const T& s){
-            return b2v(v2b(v1)*xsimd::batch<T,A>::broadcast(s));
+            auto vs= xsimd::batch<T,A>::broadcast(s);
+            return b2v(v2b(v1)*vs);
         }
 
         //scalar pre-multiplication
@@ -116,14 +123,10 @@ class alignas(ALIGN_WIDTH) vec4{
             return b2v(-v2b(v1));
         }
 
-        //indexing operation
-        constexpr T operator[](int i){
-            return m_v[i];
-        }
-        
         //print the vector
         friend std::ostream& operator<<(std::ostream& out, const vec4& v) {
-			out <<"("<< v.m_v[0] << "," << v.m_v[1] << "," << v.m_v[2]<<"," << v.m_v[3]<<")";
+            
+			out <<'('<< v.m_v[0] << ',' << v.m_v[1] << ',' << v.m_v[2]<<','<< v.m_v[3]<<')';
 			return out;
 		}
 
@@ -131,16 +134,28 @@ class alignas(ALIGN_WIDTH) vec4{
         T m_v[4];
 
         static constexpr xsimd::batch<T,A> v2b(const vec4& v){
+            /*
+            v2b: vector to batch 
+            returns: a xsimd::batch object using aligned memory access. 
+            */
             return  xsimd::batch<T,A>::load_aligned(v.m_v);
         }
         
         static constexpr vec4 b2v(xsimd::batch<T,A> x){
+            /*
+            b2v:batch to vector
+            returns: a Vec4 object using aligned memory access. 
+            */
             vec4 v{x};
             return v;
         }
 
-        static constexpr T sum(xsimd::batch<T,A> x){
-            return xsimd::batch<T,A>::hadd(x);
+        static constexpr T sum(xsimd::batch<T,A> x){ //nice alternative is xsimd::hadd but garbage vals.
+            /*
+            returns: sum of first 3 values.
+            */
+            auto b_temp=b2v(x);
+            return b_temp[0]+b_temp[1]+b_temp[2];
         }
     
 
