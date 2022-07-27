809be217c9da3c7ed6684927bee298830ba09b48
Mahesh Bhupati <67902842+mahesh0537@users.noreply.github.com>
2022-07-27 18:27:02 +0530

    Development (#28)
    * scene added
    
    * scene added
    
    * update to sync my repo

---
+++ Rendera/include/scene.hpp
@@ -0,0 +1,171 @@
+#ifndef SCENE_HPP
+#define SCENE_HPP
+
+#include "sphere.hpp"
+#include "intersection.hpp"
+#include "camera.hpp"
+
+class Hitinfo{
+    public:
+        bool ishitting = false;
+        float dist;
+        vec3 intersection_pt;
+        ray normal;
+        ray ray_;
+        color shade = {0,0,0};
+        color shade_new = {0,0,0};
+        color rgb;
+        ray ref_ray;
+};
+
+color lighting(Material mat, ray cam_ray, ray normal, vec3 point, vec3 light_src, color intensity){
+
+    ray incident_ray = {point, point - light_src};
+    ray reflect_ray = reflect(incident_ray, normal);
+
+    color effective_col = mat.Color() * intensity;
+
+    //determine ambient contribution
+    color ambient_col = effective_col * mat.amb();
+
+    
+    //determine diffuse contribution, using Lambert's cosine law
+    color diffuse_col = {0, 0, 0};
+    float diffuse_cosine = dot(-incident_ray.get_direction(), normal.get_direction());
+
+    // float lambert_cosine = dot(-cam_ray.get_direction(), normal.get_direction());
+
+    if (diffuse_cosine >= 0){
+        diffuse_col = diffuse_cosine * effective_col * mat.dif();
+    }
+
+    
+
+    float specular_cosine = dot(-cam_ray.get_direction(), reflect_ray.get_direction());
+    float spec_factor = 0;
+
+    if(specular_cosine >= 0){
+        spec_factor = pow(specular_cosine, mat.shiny());
+    }
+    color specular_col = spec_factor * intensity * mat.spe();
+
+    return ambient_col + diffuse_col + specular_col;
+}
+
+class Scene{
+    public:
+        int n_sphere;
+        uint16_t img_width;
+        uint16_t img_height;
+        Hitinfo *hitinfo = new Hitinfo[img_height*img_width];
+        sphere *hitable_sphere = new sphere[n_sphere];
+
+
+        vec3 light_src = {-5,3,5};
+        color light_col = {1,1,1};
+
+
+        vec3 cam_origin = {0,0,0};
+        float port_width = 8;
+        float port_height = 8;
+        float focal_length = 2;
+        int max_col = 255;
+        Camera cam;
+
+
+
+    
+
+
+
+
+    public:
+        Scene(int n_sphere_, uint16_t img_height_, uint16_t img_width_):n_sphere {n_sphere_}, img_height {img_height_},img_width {img_width_} {
+                    cam = Camera(cam_origin, port_width, port_height, img_width, img_height, focal_length);
+
+        }
+        ~Scene(){
+            delete hitable_sphere;
+            delete hitinfo;
+        }
+        void set_sphere(int index, vec3 center, float radius, Material mat){
+            if (index < n_sphere)
+                hitable_sphere[index] = sphere(center, radius, mat);
+            else
+                std::cout<<"accessing wrong index"<<std::endl;
+        }
+
+
+
+        void hit_object(int x, int y){
+            hitinfo[y*img_width + x].ray_ = ray(cam.get_ray(x, y));
+            for(int i = 0; i < n_sphere; i++){
+                Intersection i_;
+                bool hits = i_.hit(hitable_sphere[i], hitinfo[y*img_width + x].ray_);
+                if (hitinfo[y*img_width + x].ishitting && hits){
+                    if (hitinfo[y*img_width + x].dist > i_.dist_1()){
+                        hitinfo[y*img_width + x].dist = i_.dist_1();
+                        hitinfo[y*img_width + x].intersection_pt = hitinfo[y*img_width + x].ray_.fetch(hitinfo[y*img_width + x].dist); 
+                        hitinfo[y*img_width + x].normal = hitable_sphere[i].normal(hitinfo[y*img_width + x].intersection_pt);
+                        hitinfo[y*img_width + x].shade = lighting(hitable_sphere[i].mat(), hitinfo[y*img_width + x].ray_, hitinfo[y*img_width + x].normal, hitinfo[y*img_width + x].intersection_pt, light_src, light_col);
+                        hitinfo[y*img_width + x].rgb = hitinfo[y*img_width + x].shade.get_int();
+                        hitinfo[y*img_width + x].ref_ray = reflect(hitinfo[y*img_width + x].ray_, hitinfo[y*img_width + x].normal);
+                        }
+                }
+                else if (hits){
+                    hitinfo[y*img_width + x].ishitting = true;
+                    hitinfo[y*img_width + x].dist = i_.dist_1();
+                    hitinfo[y*img_width + x].intersection_pt = hitinfo[y*img_width + x].ray_.fetch(hitinfo[y*img_width + x].dist); 
+                    hitinfo[y*img_width + x].normal = hitable_sphere[i].normal(hitinfo[y*img_width + x].intersection_pt);
+                    hitinfo[y*img_width + x].shade = lighting(hitable_sphere[i].mat(), hitinfo[y*img_width + x].ray_, hitinfo[y*img_width + x].normal, hitinfo[y*img_width + x].intersection_pt, light_src, light_col);
+                    hitinfo[y*img_width + x].rgb = hitinfo[y*img_width + x].shade.get_int();
+                    hitinfo[y*img_width + x].ref_ray = reflect(hitinfo[y*img_width + x].ray_, hitinfo[y*img_width + x].normal);
+
+                }
+            }
+        }
+
+        void next_depth(int x, int y){
+            for(int i = 0; i < n_sphere; i++){
+                if(hitinfo[y*img_width + x].ishitting){
+                Intersection i_;
+                bool hits = i_.hit(hitable_sphere[i], hitinfo[y*img_width + x].ref_ray);
+                if (hitinfo[y*img_width + x].ishitting && hits){
+                    if (hitinfo[y*img_width + x].dist > i_.dist_1()){
+                        hitinfo[y*img_width + x].dist = i_.dist_1();
+                        hitinfo[y*img_width + x].intersection_pt = hitinfo[y*img_width + x].ref_ray.fetch(hitinfo[y*img_width + x].dist); 
+                        hitinfo[y*img_width + x].normal = hitable_sphere[i].normal(hitinfo[y*img_width + x].intersection_pt);
+                        hitinfo[y*img_width + x].shade_new = lighting(hitable_sphere[i].mat(), hitinfo[y*img_width + x].ref_ray, hitinfo[y*img_width + x].normal, hitinfo[y*img_width + x].intersection_pt, light_src, light_col);
+                        hitinfo[y*img_width + x].ref_ray = reflect(hitinfo[y*img_width + x].ray_, hitinfo[y*img_width + x].normal);
+                        }
+                }
+                }
+            }
+        }
+
+        void render(){
+            for(int x=0; x<img_width; x++){
+                for(int y=0; y<img_height; y++){
+                    hit_object(x,y);
+                    // hitinfo[y*img_width + x].ishitting = false;
+                    next_depth(x,y);
+                    hitinfo[y*img_width + x].shade += hitinfo[y*img_width + x].shade_new;
+                    hitinfo[y*img_width + x].shade_new = {0,0,0};
+                    hitinfo[y*img_width + x].rgb = hitinfo[y*img_width + x].shade.get_int();
+                    // next_depth(x,y);
+                    next_depth(x,y);
+                    hitinfo[y*img_width + x].shade += hitinfo[y*img_width + x].shade_new;
+                    hitinfo[y*img_width + x].shade_new = {0,0,0};
+                    hitinfo[y*img_width + x].rgb = hitinfo[y*img_width + x].shade.get_int();                    
+                   
+                    }
+                }
+            }
+
+
+};
+
+
+
+
+#endif
--- Rendera/src/main.cpp
+++ Rendera/src/main.cpp
@@ -4,100 +4,54 @@
 #include "camera.hpp"
 #include "sphere.hpp"
 #include "intersection.hpp"
+#include "scene.hpp"
 #include <fstream>
 #include <cmath>
 #include <random>
 
-color lighting(Material mat, ray cam_ray, ray normal, vec3 point, vec3 light_src, color intensity){
 
-    ray incident_ray = {point, point - light_src};
-    ray reflect_ray = reflect(incident_ray, normal);
 
-    color effective_col = mat.Color() * intensity;
 
-    //determine ambient contribution
-    color ambient_col = effective_col * mat.amb();
-
-    
-    //determine diffuse contribution, using Lambert's cosine law
-    color diffuse_col = {0, 0, 0};
-    float diffuse_cosine = dot(-incident_ray.get_direction(), normal.get_direction());
-
-    // float lambert_cosine = dot(-cam_ray.get_direction(), normal.get_direction());
-
-    if (diffuse_cosine >= 0){
-        diffuse_col = diffuse_cosine * effective_col * mat.dif();
-    }
-
-    
-
-    float specular_cosine = dot(-cam_ray.get_direction(), reflect_ray.get_direction());
-    float spec_factor = 0;
-
-    if(specular_cosine >= 0){
-        spec_factor = pow(specular_cosine, mat.shiny());
-    }
-    color specular_col = spec_factor * intensity * mat.spe();
-
-    return ambient_col + diffuse_col + specular_col;
-}
 
 int main(){
+    int n_sphere = 3;
+    int img_width = 5000;
+    int img_height = 5000;
 
-    vec3 center = {5,0,0};
-    color blue = {0.2,0.2, 1};
-    Material mat_1 ={blue, 0.1, 1, 0.9, 200};
-    sphere s(center,4, mat_1);
+    vec3 center = {3,-2,0};
+    vec3 center1 = {3, 1, 0};
+    vec3 center2 = {5, 0, 5};
 
-    vec3 light_src = {-0.5,-3,5};
-    color light_col = {1,1,1};
+    color b = {0,0, 1};
+    color g = {0,1,0};
+    color r = {1, 0,0};
+    Material mat_1 ={b, 0.1, 1, 0.9, 200};
+    Material mat_2 = {g, 0.1, 1, 0.9, 200};
+    Material mat_3 = {0.5*r + 0.2*b, 0.1, 1, 0.9, 200};
 
-    vec3 cam_origin = {0,0,0};
-    float port_width = 8;
-    float port_height = 8;
-    int img_width = 512;
-    int img_height = 512;
-    float focal_length = 2;
+    Scene sc(n_sphere, img_height, img_width);
+    sc.set_sphere(0, center, 1, mat_1);
+    sc.set_sphere(1, center1, 1, mat_2);
+    sc.set_sphere(2, center2, 1, mat_3);
+
+    sc.render();
+
+
+
+
+
     int max_col = 255;
-    Camera cam(cam_origin, port_width, port_height, img_width, img_height, focal_length);
-
-    //ppm file
     std::ofstream image("sphere.ppm");
     image << "P3\n";
     image << img_width << " " << img_height << "\n";
     image << max_col << "\n";
 
-
-    for(int i=0; i<img_height; i++){
-        for(int j=0; j<img_width; j++){
-
-            //process each ray
-            color shade;
-            for(int sample=0; sample<50; sample++){
-
-                ray ray_1 = cam.get_ray(j,i);
-
-                Intersection i;
-                bool hits = i.hit(s,ray_1);
-                color temp_shade = {1,1,1};
-                
-                
-                if(hits){
-
-                    vec3 hit_point = ray_1.fetch(i.dist_1()); 
-                    temp_shade = lighting(s.mat(), ray_1, s.normal(hit_point),hit_point,light_src, light_col);
-                }
-
-                if(sample==0){shade = temp_shade;}
-                shade = (shade*(sample) + temp_shade)* (1.0/(sample+1));
-
-            }
-            shade = shade.get_int();
-
-            //print result
-            image << shade.get_R() << " " << shade.get_G() << " " << shade.get_B()<<" " ;
+    for(int i = 0; i < img_height*img_width ; i++){
+        if(sc.hitinfo[i].ishitting){
+            image << sc.hitinfo[i].rgb.get_R() << " " << sc.hitinfo[i].rgb.get_G() << " " << sc.hitinfo[i].rgb.get_B()<<" " ;
         }
+        else
+            image << "0 0 0 ";
     }
-
     image.close();
 }
